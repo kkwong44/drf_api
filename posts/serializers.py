833fad098b2cacbb72eabeed0a9e6a8b71ea71ba
kkwong44 <wong.k@sky.com>
2022-08-26 13:32:47 +0000

    added post list view

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 
-class ProfileSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -30,6 +30,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'profile_id', 'profile_image', 'created_at',
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at',
             'updated_at', 'title', 'content', 'image', 'image_filter'
         ]
